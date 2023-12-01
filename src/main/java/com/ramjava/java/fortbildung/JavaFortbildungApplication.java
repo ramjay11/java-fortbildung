161733948baea90e91c36c77cebce286ba13cd7e
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2023-12-02 04:05:04 +0800

    Konstruktor-Commit

---
+++ src/main/java/com/ramjava/java/fortbildung/JavaFortbildungApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.java.fortbildung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFortbildungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFortbildungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramjava/java/fortbildung/JavaFortbildungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.java.fortbildung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFortbildungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
